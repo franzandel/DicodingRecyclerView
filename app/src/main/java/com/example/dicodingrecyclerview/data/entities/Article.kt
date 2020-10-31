00cfd57f12ba0268b72b2d79e6be0fd95be7ccf9
ivanaazuka <ivana.situmorang@amarbank.co.id>
2020-10-31 22:25:41 +0700

    refactor(article) : adjust code style

--- app/src/main/java/com/example/dicodingrecyclerview/data/entities/Article.kt
+++ app/src/main/java/com/example/dicodingrecyclerview/data/entities/Article.kt
@@ -5,12 +5,12 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class Article(
-    val author: String ?= null,
-    val content: String ?= null,
-    val description: String ?= null,
-    val publishedAt: String ?= null,
-    val source: Source?= null,
-    val title: String ?= null,
-    val url: String ?= null,
-    val urlToImage: String ?= null
+    val author: String? = null,
+    val content: String? = null,
+    val description: String? = null,
+    val publishedAt: String? = null,
+    val source: Source? = null,
+    val title: String? = null,
+    val url: String? = null,
+    val urlToImage: String? = null
 ) : Parcelable
